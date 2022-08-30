30cc750a04ac3e45bef64db7d23dd8271e9b6d22
Luis Paz Rumiche <pazrumicheluis@gmail.com>
2022-08-30 10:59:05 -0500

    actualizando propiedad de prueba

--- accounts/src/main/java/com/bootcamp/bank/accounts/controller/CateogoryController.java
+++ accounts/src/main/java/com/bootcamp/bank/accounts/controller/CateogoryController.java
@@ -1,6 +1,7 @@
 package com.bootcamp.bank.accounts.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +9,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 @RestController
-@RequestMapping("/accounts")
+@RequestMapping("/category")
 @Getter
 @Setter
-public class AccountsController {
+public class CateogoryController {
 
 	@Value("${app.testProp}")
 	private String testProp;
 	
+	@GetMapping
+	public String getTestProp() {
+		return this.testProp;
+	}
 }
