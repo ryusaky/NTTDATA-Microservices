3a78a146c8ed483b5e552ab6aef1c04a23fcae8e
Luis Paz Rumiche <pazrumicheluis@gmail.com>
2022-08-30 11:06:17 -0500

    actualizando propiedad para validar el refreshscope de configuration cliente.

--- accounts/src/main/java/com/bootcamp/bank/accounts/controller/CategoryController.java
+++ accounts/src/main/java/com/bootcamp/bank/accounts/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.bootcamp.bank.accounts.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,8 @@ import lombok.Setter;
 @RequestMapping("/category")
 @Getter
 @Setter
-public class CateogoryController {
+@RefreshScope
+public class CategoryController {
 
 	@Value("${app.testProp}")
 	private String testProp;
