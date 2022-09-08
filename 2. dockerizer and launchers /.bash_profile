30b414afd1e56548eb1bdc126c0924103b8c778d
Luis Paz Rumiche <pazrumicheluis@gmail.com>
2022-09-07 19:30:39 -0500

    update docker config

---
+++ 2. dockerizer and launchers /.bash_profile
@@ -0,0 +1,3 @@
+export M2_HOME="/Users/ryusaky/Applications/apache-maven-3.8-2.6"
+PATH="${M2_HOME}/bin:${PATH}"
+export PATH
--- 2. dockerizer and launchers /config-server.sh
+++ 2. dockerizer and launchers /config-server.sh
@@ -1,10 +1,12 @@
-#1. posicionando en la carpeta del proyecto
+#1.- Configurando Maven
+source .bash_profile
+#2.- Posicionando en la carpeta del proyecto
 cd ../config-server
-#2. compilar y generar el archivo .jar que será el launcher de la aplicación en docker
+#3.- Compilar y generar el archivo .jar que será el launcher de la aplicación en docker
 mvn clean install
 echo "JAR generado"
-#3. Containerización de la aplicación, utilizando el archivo Dockerfile
+#4.- Containerización de la aplicación, utilizando el archivo Dockerfile
 docker build -t config-server .
 echo "IMAGEN creada"
-#4. Ejecución del contenedor especificandole el puerto que tendrá habilitado
-docker run -p 8761:8761 config-server
+#5.- Ejecución del contenedor especificandole el puerto que tendrá habilitado
+docker run -p 8888:8888 config-server
